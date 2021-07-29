05f0804e452159d324eb96a43bb2faf35b4dafe8
JIN-076 <andy230@naver.com>
2021-07-29 18:17:27 +0900

    [Piscine Python Django] Day01 Complete

---
+++ Piscine_Python_Django/Day01/ex04/state.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+import sys
+
+def dict_get_key_from_value(dict: dict, value):
+    for key, item in dict.items():
+        if item == value:
+            return key
+        return None
+
+def print_state(value: str):
+    states = {
+        "Oregon": "OR",
+        "Alabma": "AL",
+        "New Jersey": "NJ",
+        "Colorado": "CO"
+    }
+    capital_cities = {
+        "OR": "Salem",
+        "AL": "Montgomery",
+        "NJ": "Ternton",
+        "CO": "Denver"
+    }
+
+    value = dict_get_key_from_value(capital_cities, value)
+    if not value:
+        print("Unknown capital city")
+        return
+    print(dict_get_key_from_value(states, value))
+
+def main():
+    if len(sys.argv) == 2:
+        print_state(sys.argv[1])
+
+if __name__ == '__main__':
+    main()
